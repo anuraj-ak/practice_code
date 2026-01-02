f59728e5648b362f2f8f7cb3c0d1b09d037b87cc
anuraj-ak <129406166+anuraj-ak@users.noreply.github.com>
2026-01-02 15:30:00 +0530

    Create pr3.java

---
+++ pr3.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class pr3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.next();
+        System.out.println("hello " + a + " have good day");// string concatation
+    }
+}
