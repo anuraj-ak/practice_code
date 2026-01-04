801fc55098982ebdbd9844a4ce2e5d6ee673511c
anuraj-ak <kalekaranuraj@gmail.com>
2026-01-04 13:17:39 +0530

    modified code

--- internship_1.java
+++ internship_1.java
@@ -1,14 +1,30 @@
 import java.util.*;
 public class internship_1{
     public static void main(String[] args) {
-        int age;
+        //int age;
         Scanner sc=new Scanner(System.in);
-        System.out.println("enter age");
-        age=sc.nextInt();
-        if(age>18){
-            System.out.println("adult");
-        }else{
-            System.out.println("not adult");
+        // System.out.println("enter age");
+        // age=sc.nextInt();
+        // if(age>18){
+        //     System.out.println("adult");
+        // }else{
+        //     System.out.println("not adult");
+        // }
+                System.out.println("enter your chice:  ");
+
+        int button =sc.nextInt();
+        switch(button){
+
+            case 1:System.out.println("hello");
+            break;
+
+            case 2:System.out.println("namaste");
+            break;
+
+            case 3:System.out.println("hii");
+            break;
+
+            default:System.out.println("invalid choice");
         }
     }
 }
