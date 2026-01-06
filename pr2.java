fb0ccceb855bb5a2febb670330c2b2a44751574e
anuraj-ak <kalekaranuraj@gmail.com>
2026-01-06 23:14:47 +0530

    first commit

---
+++ pr2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class pr2 {
+    public static void main(String[] args) {
+        
+    Scanner sc=new Scanner(System.in);
+    float sub1=sc.nextInt();
+    float sub2=sc.nextInt();
+    float sub3=sc.nextInt();
+    float sub4=sc.nextInt();
+    float result=(sub2+sub1+sub3+sub4)/40;
+    System.out.println(result);
+    
+    }
+}
+
