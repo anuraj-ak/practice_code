c2a660c61fb26f1131bd0e980cf95f7c2eaf9699
anuraj-ak <kalekaranuraj@gmail.com>
2026-01-12 23:28:54 +0530

    initial commit

---
+++ core_java/loops/loops.java
@@ -0,0 +1,11 @@
+package practice_code.core_java.loops;
+
+public class loops {
+    public static void main(String[]args){
+    int counter=1;
+    while(counter>0){
+        System.out.println("om nam shivay");
+        counter++;
+    }
+    }
+}
